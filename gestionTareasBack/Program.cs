d5240ddd8be7dcc458ec591448814fbb4e3a7a7c
yeison6 <yeison@innclod.com>
2025-03-15 14:03:52 -0500

    add azure

--- gestionTareasBack/Program.cs
+++ gestionTareasBack/Program.cs
@@ -25,9 +25,9 @@ builder.Services.AddScoped<ITaskService, TaskService>();
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowSpecificOrigins", policy =>
+    options.AddPolicy("AllowAll", policy =>
     {
-        policy.WithOrigins("http://localhost:4200")
+        policy.AllowAnyOrigin()
         .AllowAnyHeader()
         .AllowAnyMethod();
     });
@@ -37,13 +37,10 @@ var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
-app.UseCors("AllowSpecificOrigins");
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseCors("AllowAll");
 
 app.UseHttpsRedirection();
 
